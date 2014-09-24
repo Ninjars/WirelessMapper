5b9694c4f638968a141e694cd7d6fbc4403185c3
Jeremy Stevens <jeremy@sixtostart.com>
2014-09-24 18:07:17 +0100

    All actors will be expected to have a position, so we can use it as an abstract class to compare relative positions between any actors.

--- src/com/ninjarific/wirelessmapper/entities/actors/RootActor.java
+++ src/com/ninjarific/wirelessmapper/entities/actors/RootActor.java
@@ -1,6 +1,18 @@
 package com.ninjarific.wirelessmapper.entities.actors;
 
+import android.graphics.PointF;
+
 public abstract class RootActor {
+	private PointF mPosition;
+
 	public abstract void update(long timeDeltaMs);
 
+
+	public PointF getPosition() {
+		return mPosition;
+	}
+
+	public void setPosition(PointF position) {
+		mPosition = position;
+	}
 }
