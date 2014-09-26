9711221443c9d27808a4934ca6b7ecdd295537bf
Jeremy Stevens <jeremy@sixtostart.com>
2014-09-26 18:27:42 +0100

    raised the scann connection threshold

--- src/com/ninjarific/wirelessmapper/Constants.java
+++ src/com/ninjarific/wirelessmapper/Constants.java
@@ -4,7 +4,7 @@ import android.graphics.Color;
 
 public class Constants {
 	public static final boolean DEBUG = true;
-	public static final int SCAN_CONNECTION_THREASHOLD = 20;
+	public static final int SCAN_CONNECTION_THREASHOLD = 30;
 	public static final int POINT_LEVEL_SIGNIFICANT_VARIATION = 20;
 
 	/*
