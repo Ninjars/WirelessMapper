e0ec2347ff919fe3ebac52f312012766930c520f
Jez <jez0707@gmail.com>
2014-10-09 23:42:23 +0100

    force connection returns correctly inverted force for corresponding actor

--- src/com/ninjarific/wirelessmapper/engine/forces/ForceConnection.java
+++ src/com/ninjarific/wirelessmapper/engine/forces/ForceConnection.java
@@ -9,17 +9,24 @@ public abstract class ForceConnection {
 	protected ForceActor mActor1;
 	protected ForceActor mActor2;
 	private PointF mForce;
+	private PointF mInverseForce;
 	private boolean mUpdateAlternator = true;
 
 	public ForceConnection(ForceActor actor1, ForceActor actor2) {
 		mActor1 = actor1;
 		mActor2 = actor2;
 		mForce = new PointF();
+		mInverseForce = new PointF();
 	}
 	
-	public PointF getForce() {
+	public PointF getForce(ForceActor actor) {
 		updateForce();
-		return mForce;
+		if (actor.equals(mActor2)) {
+			return mForce;
+		} else {
+			mInverseForce.set(-mForce.x, -mForce.y);
+			return mInverseForce;
+		}
 	}
 	
 	public ForceActor getActor1() {
